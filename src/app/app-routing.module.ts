fa677b73d46895d617f1f09e346aa4328746a0f0
Vitor Trimer <vitortrimer@gmail.com>
2020-10-25 18:55:32 -0300

    connect with database

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { NewListComponent } from './pages/new-list/new-list.component';
 import { TaskViewComponent } from './pages/task-view/task-view.component';
 
 const routes: Routes = [
-  { path: '', component: TaskViewComponent }
+  { path: '', redirectTo: 'lists', pathMatch: 'full' },
+  { path: 'new-list', component: NewListComponent },
+  { path: 'lists', component: TaskViewComponent},
+  { path: 'lists/:listId', component: TaskViewComponent}
 ];
 
 @NgModule({
--- src/app/pages/task-view/task-view.component.ts
+++ src/app/pages/task-view/task-view.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { TaskService } from 'src/app/task.service';
 
 @Component({
   selector: 'app-task-view',
@@ -7,9 +9,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TaskViewComponent implements OnInit {
 
-  constructor() { }
+  lists: any[];
+  tasks: any[];
+
+  constructor(private taskService: TaskService, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.route.params.subscribe(
+      (params: Params) => {
+        this.taskService.getTasks(params.listId).subscribe((tasks: any[]) => {
+          this.tasks = tasks
+        })
+      }
+
+    )
+
+    this.taskService.getLists().subscribe((lists: any[]) => {
+      this.lists = lists;
+    })
   }
 
 }
